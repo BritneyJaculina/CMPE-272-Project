172cc52a2193ca2cc139817fe28a09d40cd4fa79
bjaculina <99229229+bjaculina@users.noreply.github.com>
2024-11-08 12:22:21 -0800

    initial project

---
+++ src/main/java/com/example/cmpe272project/Cmpe272ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cmpe272project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cmpe272ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cmpe272ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cmpe272project/Cmpe272ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cmpe272project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cmpe272ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
